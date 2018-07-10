8cce8fdb7f84177cf1d08363ddf3313c2172942c
farizsamanha <farizsamanha@gmail.com>
2018-07-10 11:08:31 +0700

    mm

--- application/controllers/C_ba.php
+++ application/controllers/C_ba.php
@@ -14,29 +14,9 @@ class C_ba extends CI_Controller {
 		$this->load->view('home_ba');
 	}
 
-	public function cetak(){
-		$data['judul'] = 'Penyerahan Laptop Dell dari IT ke Fasup West';
-		$data['tanggal'] = '22-12-2000';
-		$data['lokasi'] = 'BSD City';
-		$data['nama'] = 'Rony F Ruzain ';
-		$data['nik'] = '81256';
-		$data['jabatan'] = 'SPV IT Operation Support Jabotabek';
-		$data['keterangan'] = 'Pihak 1';
-		$data['nama2'] = 'Hasnawita ';
-		$data['nik2'] = '71350';
-		$data['jabatan2'] = 'Supervisor Facilities Support Western Jabotabek';
-		$data['keterangan2'] = 'Pihak 2';
-		$data['statement'] = 'Dengan ini menyatakan “ telah dilakukan pengeluaran 16 unit Laptop Dell dari lokasi Office Telkomsel BSD ke Fasup West an Hasnawita”. Unit tersebut dengan rincian sebagai berikut.';
-		$data['lokasi_asal'] = 'BSD Siti';
-		$data['lokasi_tujuan'] = 'Siti BSD';
-		$this->load->view('cetak2', $data);
-	}
-
 	public function printpdf($hasil){
-		//var_dump($query);
 		$html2pdf = new Html2Pdf('P', 'A4', 'en');
 		$html2pdf->pdf->SetMargins(20, 5, 20);
-        //$hasil = file_get_contents(base_url('index.php/C_ba/printdata?'.$query));
         $html2pdf->pdf->SetMargins(20, 5, 20);
         $html2pdf->pdf->SetFont('Times', '', 10);
 		$html2pdf->pdf->AddPage(); 
@@ -60,9 +40,7 @@ class C_ba extends CI_Controller {
 		$data['keterangan2'] = $this->input->post('keterangan2');
 		$data['statement'] = $this->input->post('statement');
 		$data['lokasi_asal'] = $this->input->post('lokasi_asal');
-		$data['lokasi_tujuan'] = $this->input->post('lokasi_tujuan');
-
-		
+		$data['lokasi_tujuan'] = $this->input->post('lokasi_tujuan');		
 
 		$arr_sn_barang = $this->input->post("sn_barang");
 	    $arr_tipe_barang = $this->input->post("tipe_barang");
@@ -78,33 +56,9 @@ class C_ba extends CI_Controller {
 	    return $this->load->view('cetak2',$data,true);
 	}
 
-	public function printdata(){
-		var_dump($this->input->get());
-		$data = array();
-		$data['judul'] = $this->input->get('judul');
-		$data['tanggal'] = $this->dates->change_format($this->input->get('tanggal'));
-		$data['lokasi'] = $this->input->get('lokasi');
-		$data['nama'] = $this->input->get('nama');
-		$data['nik'] = $this->input->get('nik');
-		$data['jabatan'] = $this->input->get('jabatan');
-		$data['keterangan'] = $this->input->get('keterangan1');
-		$data['nama2'] = $this->input->get('nama2');
-		$data['nik2'] = $this->input->get('nik2');
-		$data['jabatan2'] = $this->input->get('jabatan2');
-		$data['keterangan2'] = $this->input->get('keterangan2');
-		$data['statement'] = $this->input->get('statement');
-		$data['lokasi_asal'] = $this->input->get('lokasi_asal');
-		$data['lokasi_tujuan'] = $this->input->get('lokasi_tujuan');
-		$data['isi'] = $this->input->get('isi'); 
-		   	    
-	    //print_r($data['isi'][0]);
-	    
-
-	}
 
 	public function result(){
 		$data = $this->postdata();	
-		//$query = http_build_query($data);
 		$this->printpdf($data);
 	}
 
